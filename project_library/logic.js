57829dc64485e1f188185ceb2ecd4db2d5e2a943
Stroud <Dakota.Stroud@ngc.com>
2024-12-09 15:27:17 -0600

    working on library project

--- project_library/logic.js
+++ project_library/logic.js
@@ -1,7 +1,14 @@
-const myLibrary = [];
+const left = document.getElementById("left-arrow");
+const right = document.getElementById("right-arrow");
 
-function Book() {
+const add = document.getElementById("add-book");
+const remove = document.getElementById("remove-book");
 
+let myLibrary = [];
+
+function Book(bookName, authorName) {
+    this.name = bookName;
+    this.author = authorName;
 }
 
 function addBook() {
@@ -12,6 +19,21 @@ function removeBook() {
 
 }
 
-function displayBook() {
-    
-}
+left.addEventListener("click", function () {
+
+})
+
+right.addEventListener("click", function () {
+
+})
+
+add.addEventListener("click", function () {
+    let bookName = prompt("What is the name of the book?");
+    let author = prompt("What is the name of the author?");
+    const newBook = new Book(bookName, author);
+    myLibrary.push(newBook);
+})
+
+remove.addEventListener("click", function () {
+
+})
