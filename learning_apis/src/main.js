da8ceed15231f53f032f3f6a1e2f3fccbd0f472b
Dakota Stroud <str.dak97@gmail.com>
2025-02-11 20:31:47 -0600

    completed api project

--- learning_apis/src/main.js
+++ learning_apis/src/main.js
@@ -1,1 +1,43 @@
 import "./styles.css";
+import { generate, count } from "random-words";
+import { apiKeys } from "../../api_keys";
+
+const giphyAPI = apiKeys.get('giphy');
+const searchButton = document.getElementById('searchButton');
+const randomButton = document.getElementById('randomButton');
+const searchText = document.getElementById('searchText');
+const giphyImg = document.getElementById('giphyImg');
+
+searchText.value = 'cats';
+
+async function getImgData() {
+    try {
+        fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${giphyAPI}&s=${searchText.value}`, {mode: 'cors'})
+        .then(function(response) {
+            return response.json();
+          })
+        .then(function(response) {
+            console.log(response);
+            if(response.data.length === 0) {
+                giphyImg.src = "";
+                alert('ERROR');
+            } 
+            else {
+                giphyImg.src = response.data.images.original.url;
+            }
+        });
+    }
+    catch(error) {
+        console.log(error);
+    }
+}
+
+randomButton.onclick = function() {
+    searchText.value = generate();
+}
+
+searchButton.onclick = function() {
+    getImgData();
+};
+
+getImgData();
