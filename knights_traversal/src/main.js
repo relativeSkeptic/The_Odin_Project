23073e173738bcddf6a632f865cef57d2af1435e
Dakota Stroud <str.dak97@gmail.com>
2025-07-01 21:50:41 -0500

    updating knight moves function

--- knights_traversal/src/main.js
+++ knights_traversal/src/main.js
@@ -1,9 +1,34 @@
 import "./styles.css";
 
-//Represents the start and end of the chess board 
-const MIN = 0;
-const MAX = 7;
+//Possible moves the knight can make stored in an array
+let possibleMoves = [
+  [+2, +1], [+1, +2],
+  [-1, +2], [-2, +1],
+  [-2, -1], [-1, -2],
+  [+1, -2], [+2, -1]
+];
 
 function knightMoves(startPos, endPos) {
+    let shortestPath = [];
+    let currentPath = [];
+    knightMovesHelper(startPos, endPos, shortestPath, currentPath);
+    return shortestPath; 
+}
 
+function knightMovesHelper(startPos, endPos, short, curr) {
+    if(startPos === endPos) {
+        //Ensure that the shortest path is stored in the short array before returning
+        if (short.length === 0 || curr.length < short.length) {
+            short.length = 0;
+            for (let position of curr) {
+                short.push(position);
+            }
+        }
+        return;
+    }
+
+    for(let nextMove of possibleMoves) {
+        let nextX = startPos[0] + nextMove[0];
+        let nextY = startPos[1] + nextMove[1];
+    }
 }
