07d7521352b67b2c4c8e287b1e38fc8e484d9bea
Dakota Stroud <str.dak97@gmail.com>
2025-07-03 15:11:42 -0500

    finished knights traversal problem

--- knights_traversal/src/main.js
+++ knights_traversal/src/main.js
@@ -1,34 +1,53 @@
 import "./styles.css";
 
-//Possible moves the knight can make stored in an array
-let possibleMoves = [
-  [+2, +1], [+1, +2],
-  [-1, +2], [-2, +1],
-  [-2, -1], [-1, -2],
-  [+1, -2], [+2, -1]
+const possibleMoves = [
+    [+2, +1], [+1, +2],
+    [-1, +2], [-2, +1],
+    [-2, -1], [-1, -2],
+    [+1, -2], [+2, -1]
 ];
 
-function knightMoves(startPos, endPos) {
-    let shortestPath = [];
-    let currentPath = [];
-    knightMovesHelper(startPos, endPos, shortestPath, currentPath);
-    return shortestPath; 
-}
+export function knightMoves(startPos, endPos) {
+    //Creating a queue which will contain all paths we need to explore
+    let queue = [[startPos, [startPos]]];
 
-function knightMovesHelper(startPos, endPos, short, curr) {
-    if(startPos === endPos) {
-        //Ensure that the shortest path is stored in the short array before returning
-        if (short.length === 0 || curr.length < short.length) {
-            short.length = 0;
-            for (let position of curr) {
-                short.push(position);
+    //Set used to store visited squares
+    let visited = new Set();
+    visited.add(startPos.toString());
+
+    //Begin looping through the queue checking each subpath as we go
+    //This will dynamically grow the queue to add additional paths to take
+    for(let i = 0; i < queue.length; i++) {
+
+        //Extract values needed from the queue
+        let [currentPos, path] = queue[i];
+
+        //Check to see if we have reached the endPos
+        if(currentPos[0] === endPos[0] && currentPos[1] === endPos[1]) {
+            return path;
+        }
+
+        //If we haven't reached the endPos yet then loop through all possible legal moves 
+        //adding all legal moves to the queue to be validated as well
+        for(const moves of possibleMoves) {
+            //New move to add to queue
+            let newX = currentPos[0] + moves[0];
+            let newY = currentPos[1] + moves[1];
+
+            //Check to ensure the move is valid
+            if(newX < 0 || newX > 7 || newY < 0 || newY > 7) {
+                continue;
+            }
+
+            //Check to ensure we have not already visited that square in our queue
+            let newPos = [newX, newY];
+            let key = newPos.toString();
+
+            //We have not visited this square so add it to the set and push it into our queue
+            if(!visited.has(key)) {
+                visited.add(key);
+                queue.push([newPos, [...path, newPos]]);
             }
         }
-        return;
-    }
-
-    for(let nextMove of possibleMoves) {
-        let nextX = startPos[0] + nextMove[0];
-        let nextY = startPos[1] + nextMove[1];
     }
 }
--- knights_traversal/src/test.js
+++ knights_traversal/src/test.js
@@ -1,1 +1,22 @@
 import { knightMoves } from "./main";
+
+let startPos = [0, 0];
+let endPos = [7, 7];
+
+let moves = knightMoves(startPos, endPos);
+
+console.log(moves);
+
+startPos = [3, 5];
+endPos = [2, 2];
+
+moves = knightMoves(startPos, endPos);
+
+console.log(moves);
+
+startPos = [1, 4];
+endPos = [6, 2];
+
+moves = knightMoves(startPos, endPos);
+
+console.log(moves);
