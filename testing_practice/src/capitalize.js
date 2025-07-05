99f6f0e9c94f872133c1d48108f05b18549f8062
Dakota Stroud <str.dak97@gmail.com>
2025-07-04 22:13:42 -0500

    finished capitalize testing

--- testing_practice/src/capitalize.js
+++ testing_practice/src/capitalize.js
@@ -1,4 +1,7 @@
 function capitalize(word) {
+    if(word === "") {
+        return null;
+    }
     return word.charAt(0).toUpperCase() + word.slice(1);
 }
 
--- testing_practice/src/capitalize.test.js
+++ testing_practice/src/capitalize.test.js
@@ -1,5 +1,11 @@
 const { capitalize } = require('./capitalize');
 
+//Base case
 test("Takes 'cat' and converts it to 'Cat'", () => {
     expect(capitalize("cat")).toMatch(/Cat/);
 })
+
+//Empty string
+test("Takes '' and returns null", () => {
+    expect(capitalize('')).toBeNull();
+});
