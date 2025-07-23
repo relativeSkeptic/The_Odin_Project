e5ad8ed061c07db8a9cde5c2d73f8dc3cdfc1d81
Dakota Stroud <str.dak97@gmail.com>
2025-07-23 08:39:58 -0500

    finished up representing ships on the gameboard

--- battleship/src/main.js
+++ battleship/src/main.js
@@ -1,13 +1,6 @@
 import "./styles.css";
-import { Player } from "./player"
-import { Computer } from "./computer"
+import { GameDriver } from "./driver"
 import { UserInterface } from "./userInterface"
 
-let UI = new UserInterface();
-UI.generateBoards();
-
-let player = new Player();
-let computer = new Computer();
-
-console.log(player.gameboard);
-console.log(computer.gameboard);
+let gameDriver = new GameDriver();
+let UI = new UserInterface(gameDriver);
--- battleship/src/userInterface.js
+++ battleship/src/userInterface.js
@@ -1,11 +1,25 @@
 export class UserInterface {
-    constructor() {
+    constructor(driver) {
+        this.gameDriver = driver;
         this.playerGrid = document.querySelector('#player');
         this.computerGrid = document.querySelector('#computer');
+        this.#generateBoards();
+        this.#setupEventListeners();
+        this.placeShips();
+    }
+
+    //Sets up the event listeners for all of the buttons
+    #setupEventListeners() {
+
+    }
+
+    //Starts the game when the user clicks the start game button
+    #startGame() {
+
     }
 
     //Generates both the computer and player boards
-    generateBoards() {
+    #generateBoards() {
         this.#createGrid(document.getElementById("computer"));
         this.#createGrid(document.getElementById("player"));
     }
@@ -22,17 +36,48 @@ export class UserInterface {
         }
 
         //Loop through all of the X and Y coordinates to create the grid and assign the correct values to it
-        for (let y = 0; y < boardLength; y++) {
-            for (let x = 0; x < boardLength; x++) {
+        for (let x = 10; x != 0; x--) {
+            for (let y = 10; y != 0; y--) {
                 const cell = document.createElement('div');
-                const coord = `${letters[y]}${x + 1}`;
+                const coord = `${letters[y]}${x}`;
                 cell.id = `${boardOwner}-${coord}`;
                 cell.dataset.x = x;
                 cell.dataset.y = y;
                 cell.dataset.owner = boardOwner;
-                cell.className = 'w-full h-full border-2 bg-blue-200 hover:bg-blue-300 cursor-pointer';
+                cell.className = 'board-cell board-cell:hover';
                 container.appendChild(cell);
             }
+        }
+    }
+
+    //Place the actual ships on the gameboard
+    placeShips() {
+        const playerShipMap = this.gameDriver.player.gameboard.shipsToCoords;
+        const computerShipMap = this.gameDriver.computer.gameboard.shipsToCoords;
+
+        console.log(playerShipMap);
+        console.log(computerShipMap);
+
+        this.#renderShips(playerShipMap, this.playerGrid);
+        this.#renderShips(computerShipMap, this.computerGrid);
+    }
+
+    #renderShips(shipMap, container) {
+        for (const coords of shipMap.values()) {
+            coords.forEach(([x, y], index) => {
+                const cell = container.querySelector(`[data-x="${x}"][data-y="${y}"]`);
+                if (!cell) return;
+
+                const shipSegment = document.createElement('div');
+                if(index === 0 || index === coords.length -1) {
+                    shipSegment.className = 'ship-end';
+                }
+                else {
+                    shipSegment.className = 'ship-body';
+                }
+
+                cell.appendChild(shipSegment);
+            });
         }
     }
 }
