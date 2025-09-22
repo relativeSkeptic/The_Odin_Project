fad7dc791daa258e1cffd57437eaddf2036ea092
Dakota Stroud <str.dak97@gmail.com>
2025-09-22 05:11:11 -0500

    rubiks cube

--- rubiks_cube/src/main.js
+++ rubiks_cube/src/main.js
@@ -1,1 +1,19 @@
 import "./styles.css";
+
+//Current rotation in degrees
+let currentRotation = 0;
+
+//Obtain a list of all squares
+const squares = document.querySelectorAll('.square');
+
+function rotateSquare() {
+    currentRotation += 1;
+    for (let square of squares) {
+        square.style.transform = `rotate(${currentRotation}deg)`;
+    }
+    if (currentRotation === 360) {
+        currentRotation = 0;
+    }
+}
+
+let interval = setInterval(rotateSquare, 1);
